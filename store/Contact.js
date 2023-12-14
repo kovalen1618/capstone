7c9e348227f892db469ec7e4b9bc752c936ac0b1
kovalen1618 <kovalen1.618@gmail.com>
2023-12-14 16:40:12 -0600

    Transforms Priora into a state-driven SPA
    Fixes collapsible navbar bug due to changing DOM
    BUG: Navigo url not changing on page reload

---
+++ store/Contact.js
@@ -0,0 +1,4 @@
+export default {
+  header: "Contact Page",
+  view: "Contact"
+};
---
+++ store/Links.js
@@ -0,0 +1,42 @@
+import logoImage from "../documents/img/logo.png";
+
+export default [
+  {
+    title: "Home",
+    text: "",
+    style: "",
+    icon: `<h1 id="header">
+      <img id="logo" src="${logoImage}" alt="Priora Logo" />
+    </h1>`
+  },
+  {
+    title: "Create",
+    text: `<span class="menu-text">Create Task</span>`,
+    style: "menu-link",
+    icon: `<i class="fa-solid fa-circle-plus"></i>`
+  },
+  {
+    title: "Badges",
+    text: `<span class="menu-text">Badges</span>`,
+    style: "menu-link",
+    icon: `<i class="fa-solid fa-award"></i>`
+  },
+  {
+    title: "About",
+    text: `<span class="menu-text">About</span>`,
+    style: "menu-link",
+    icon: `<i class="fa-solid fa-scroll"></i>`
+  },
+  {
+    title: "Contact",
+    text: `<span class="menu-text">Contact</span>`,
+    style: "menu-link",
+    icon: `<i class="fa-solid fa-envelope"></i>`
+  },
+  {
+    title: "Settings",
+    text: `<span class="menu-text">Settings</span>`,
+    style: "menu-link",
+    icon: `<i class="fa-solid fa-gear"></i>`
+  }
+];
