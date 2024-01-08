cc8022de7a5c32b377944833a7d6c928024e0bd1
kovalen1618 <kovalen1.618@gmail.com>
2024-01-08 15:06:41 -0600

    Implements Task schema and routers backend

--- server/app.js
+++ server/app.js
@@ -1,7 +1,48 @@
 // 'Import' the Express module instead of http
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// Routers
+import tasks from "./routers/tasks.js";
+
+// Load Enviroment Variables from .env File
+dotenv.config();
+
 // Initialize the Express application
 const app = express();
+
+// Initialize Mongoose
+mongoose.connect(process.env.MONGODB, {
+  // Configuration options to remove deprecation warnings, just include them to remove clutter
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Initialize Database
+const db = mongoose.connection;
+
+// Log Database Status
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+// CORS Middleware - allows any origin to access the server
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
 
 // Body-Parsing Middleware
 const logging = (request, response, next) => {
@@ -11,6 +52,9 @@ const logging = (request, response, next) => {
   next();
 };
 
+app.use(cors);
+// Allows requests to database to use JSON
+app.use(express.json());
 app.use(logging);
 
 // Handle the request with HTTP GET method from http://localhost:4040/status
@@ -20,6 +64,8 @@ app.get("/status", (request, response) => {
   // End and return the response
   response.send(JSON.stringify({ message: "Service healthy" }));
 });
+
+app.use("/home", tasks);
 
 // Tell the Express app to start listening
 app.listen(4040, () => console.log("Listening on port 4040"));
